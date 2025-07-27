961445723bb01b90c9b66452a5269a5b8197e4de
sergigarcia32 <sergigarciaDev@outlook.com>
2025-07-27 20:48:35 +0200

    aprendizaje

--- assets/js/dataProjects.js
+++ assets/js/dataProjects.js
@@ -1,15 +1,15 @@
 const projects = [
   {
-    title: "Proyecto personal: Aprendizaje y Pasión por el Sector Inmobiliario",
+    title: "Aprendizaje y Pasión por el Sector Inmobiliario y ventas.<br>",
     description: `
-   Me dedico a aprender de forma autodidacta sobre el mundo de las ventas y las propiedades.<br>
-  Elaboro mis propios apuntes y los repaso diariamente, profundizando en información online relacionada con bienes raíces y conocimientos de informática básica.<br>
-  Cada día escucho un pódcast especializado en bienes raíces.<br>
-  Además, disfruto explorando páginas web de propiedades en distintas zonas residenciales y ubicaciones de España y Cataluña, informándome sobre los precios de viviendas y terrenos.
+   - Me dedico a aprender de forma autodidacta sobre el mundo de las ventas y las propiedades.<br>
+  - Elaboro mis propios apuntes y los repaso diariamente, profundizando en información online relacionada con bienes raíces y conocimientos de informática básica.<br>
+  - Me gusta ver vídeos de YouTube sobre ventas, marketing y propiedades, y sigo a profesionales destacados en el sector inmobiliario.<br>
+  - Además, disfruto explorando páginas web de propiedades en distintas zonas residenciales y ubicaciones de España y Cataluña, informándome sobre los precios de viviendas y terrenos.
       `,
     button: {
-      text: "Ver más",
-      link: "#",
+      text: "Vane Monroe",
+      link: "https://www.youtube.com/@VaneMonroe",
     },
   },
 ];
@@ -24,7 +24,16 @@ function renderProjects() {
           <h1 class="display-5 fw-bold">${project.title}</h1>
           <p class="col-md-8 fs-4">
             ${project.description}
+
+           
           </p>
+           <p class="lead fs-4 fw-bold">Aprendiendo sobre el sector inmobiliario, ventas, marketing y propiedades de..
+          </p>
+   
+          <a class="btn btn-dark btn-lg" href="${project.button.link}" role="button" target="_blank">
+            ${project.button.text}
+          </a>
+    
         </div>
       </div>
     `
